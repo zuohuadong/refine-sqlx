2bc26ee0d50291f9f4fbb321ac3924b35555bc8d
Seven Du <5564821+medz@users.noreply.github.com>
2025-07-09 20:20:50 +0800

    Add remaining CRUD methods to SQLx data provider

--- packages/refine-sqlx/src/data-provider.ts
+++ packages/refine-sqlx/src/data-provider.ts
@@ -1,15 +1,40 @@
 import type {
   BaseRecord,
+  CreateManyParams,
+  CreateManyResponse,
+  CreateParams,
+  CreateResponse,
   DataProvider,
+  DeleteManyParams,
+  DeleteManyResponse,
+  DeleteOneParams,
+  DeleteOneResponse,
   GetListParams,
   GetManyParams,
   GetManyResponse,
+  GetOneParams,
+  GetOneResponse,
+  UpdateManyParams,
+  UpdateManyResponse,
+  UpdateParams,
+  UpdateResponse,
 } from '@refinedev/core';
 
 type CreateDataProviderOptions = { url: string | URL };
 
 export default ({ url }: CreateDataProviderOptions): DataProvider => {
-  return { getApiUrl: undefined as any as () => string, getList, getMany };
+  return {
+    getApiUrl: undefined as any as () => string,
+    getList,
+    getMany,
+    getOne,
+    create,
+    createMany,
+    update,
+    updateMany,
+    deleteOne,
+    deleteMany,
+  };
 
   function getList<T extends BaseRecord = BaseRecord>(
     params: GetListParams,
@@ -20,6 +45,48 @@ export default ({ url }: CreateDataProviderOptions): DataProvider => {
   function getMany<T extends BaseRecord = BaseRecord>(
     params: GetManyParams,
   ): Promise<GetManyResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function getOne<T extends BaseRecord = BaseRecord>(
+    params: GetOneParams,
+  ): Promise<GetOneResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function create<T extends BaseRecord = BaseRecord, Variables = {}>(
+    params: CreateParams<Variables>,
+  ): Promise<CreateResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function createMany<T extends BaseRecord = BaseRecord, Variables = {}>(
+    params: CreateManyParams<Variables>,
+  ): Promise<CreateManyResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function update<T extends BaseRecord = BaseRecord, Variables = {}>(
+    params: UpdateParams<Variables>,
+  ): Promise<UpdateResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function updateMany<T extends BaseRecord = BaseRecord, Variables = {}>(
+    params: UpdateManyParams<Variables>,
+  ): Promise<UpdateManyResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function deleteOne<T extends BaseRecord = BaseRecord, Variables = {}>(
+    params: DeleteOneParams<Variables>,
+  ): Promise<DeleteOneResponse<T>> {
+    throw new Error('Unimplemented');
+  }
+
+  function deleteMany<T extends BaseRecord = BaseRecord, Variables = {}>(
+    params: DeleteManyParams<Variables>,
+  ): Promise<DeleteManyResponse<T>> {
     throw new Error('Unimplemented');
   }
 };
