542c4edc67e98b4c332ce0ac2b84afce0e30792e
Seven Du <5564821+medz@users.noreply.github.com>
2025-07-09 20:29:38 +0800

    Add utility function for creating CRUD sorting strings

---
+++ packages/refine-sqlx/src/_utils.ts
@@ -0,0 +1,9 @@
+import type { CrudSorting } from '@refinedev/core';
+
+export function createCrudSorting(sort: CrudSorting) {
+  if (!sort.length) return void 0;
+
+  return sort
+    .map(({ field, order }) => `${field} ${order.toUpperCase()}`)
+    .join(' ');
+}
